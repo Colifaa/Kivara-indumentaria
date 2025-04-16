392c80bf68454c923919612a9c272429c4d0b2c1
unknown <Jorgemathez14@gmail.com>
2025-04-16 16:55:20 -0300

    estilos-responsivedesign

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ParticlesBackground } from '@/components/ParticlesBackground';
+import Wsp from '@/components/Wsp';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={`${inter.className} bg-rosa-claro`} suppressHydrationWarning>
+        <Wsp/>
         <ParticlesBackground />
         {children}
       </body>
